f4c52cbd3a63ed894ce3a4e9f199affaf291b48b
pcx <p_lord@163.com>
2022-07-24 20:00:33 +0800

    测试

---
+++ src/main/java/Test.java
@@ -0,0 +1,14 @@
+package src.main.java;
+
+/**
+ * @author екохиЗ
+ * @project AdvancedAlgorithm
+ * @package src.main.java
+ * @date 2022/7/24 19:58
+ * @mail p_lord@163.com
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
